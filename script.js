c2da10e84466f7af516a42616ecc0ae7db2d8850
facetable <diofff6@gmail.com>
2022-05-27 22:33:21 +0300

    Добавил подсчет баллов, функцию генерации случайного бокса, событие, отслеживающее нажатие на бокса, отчистка поля при нажатии на бокс

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 var startBtn = document.querySelector('#start')
 var gameField = document.querySelector('#game')
 
-function renderGenerateBoxes() {
+var score = 0
 
+function renderGenerateBoxes() {
+    gameField.innerHTML = ''
+    var box = document.createElement('div')
+    box.setAttribute('data-box', 'true')
+
+    generateBoxStyles(box)
+    gameField.insertAdjacentElement('afterbegin', box)
+}
+
+function generateBoxStyles(box) {
+    box.style.height = box.style.width = '50px'
+    box.style.position = 'absolute'
+    box.style.backgroundColor = '#000'
+    box.style.top = '50px'
+    box.style.left = '60px'
+    box.style.cursor = 'pointer'
 }
 
 function startGame() {
@@ -12,4 +28,12 @@ function startGame() {
     renderGenerateBoxes()
 }
 
+function handleBoxClick(e) {
+    if (e.target.dataset.box) {
+        score++
+        renderGenerateBoxes()
+    }
+}
+
 startBtn.addEventListener('click', startGame)
+gameField.addEventListener('click', handleBoxClick)
