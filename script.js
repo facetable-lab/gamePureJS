fc06cd3fa55cffaedfca9f5a2d3e3a2659ce3a99
facetable <diofff6@gmail.com>
2022-05-27 23:05:40 +0300

    Добавил функцию генерации разницы макс. значений для генерации бокса, функцию получения псевдо-случайных чисел, генерацию нового бокса с другими размерами и местоположением

--- script.js
+++ script.js
@@ -6,18 +6,33 @@ var score = 0
 function renderGenerateBoxes() {
     gameField.innerHTML = ''
     var box = document.createElement('div')
+    var boxSize = generateBoxSize(30, 125)
+    var gameFieldSize = gameField.getBoundingClientRect()
+    var maxTop = generateLambdaNums(gameFieldSize, boxSize)[0] //gameFieldSize.height - boxSize
+    var maxLeft = generateLambdaNums(gameFieldSize, boxSize)[1] //gameFieldSize.width - boxSize
+    console.log(gameFieldSize)
+
     box.setAttribute('data-box', 'true')
 
-    generateBoxStyles(box)
+    generateBoxStyles(box, boxSize, maxTop, maxLeft)
+
     gameField.insertAdjacentElement('afterbegin', box)
 }
 
-function generateBoxStyles(box) {
-    box.style.height = box.style.width = '50px'
+function generateLambdaNums(gameFieldSize, boxSize) {
+    return [gameFieldSize.height - boxSize, gameFieldSize.width - boxSize]
+}
+
+function generateBoxSize(min, max) {
+    return Math.floor(Math.random() * (max - min) + min)
+}
+
+function generateBoxStyles(box, boxSize, maxTop, maxLeft) {
+    box.style.height = box.style.width = boxSize + 'px'
     box.style.position = 'absolute'
     box.style.backgroundColor = '#000'
-    box.style.top = '50px'
-    box.style.left = '60px'
+    box.style.top = generateBoxSize(0, maxTop) + 'px'
+    box.style.left = generateBoxSize(0, maxLeft) + 'px'
     box.style.cursor = 'pointer'
 }
 
